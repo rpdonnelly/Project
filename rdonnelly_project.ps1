50c5f04072f9ca398789435bf7e6e58727aca3b6
rpdonnelly <rpdonnelly@gmail.com>
2016-06-08 20:21:40 -0400

    v1
    v1

---
+++ rdonnelly_project.ps1
@@ -0,0 +1,14 @@
+﻿#Author: Ryan Donnelly
+#Date 6/8/16
+#Purpose: Interview Project that will generate a csv report of all instances in a defined VPC, or all VPC if left blank.
+#It will display the VPC ID, Instance ID, Private IP address, and all attached security groups; pipe-delimited if multiple groups are attached to an instance.
+#Requirements:  This must be run from a windows computer within your AWS environment, or a local machine that has access to your AWS account, with the aws sdk cmdlets loaded.
+#This script assumes you have a powershell profile defined that has appropriate credentials (access key and shared secret) with rights to read ec2 information.
+
+$vpcid = '*'
+$input = Read-Host -Prompt 'Input your VPC ID; for all VPC leave blank:'
+
+if ($input -ne '') { $vpcid = $input }
+
+$MyInstances = (Get-EC2Instance -Filter @{Name="vpc-id"; Values=$vpcid} ).Instances 
+
